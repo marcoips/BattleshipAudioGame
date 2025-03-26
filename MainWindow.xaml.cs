ec18e315e6012b713def6866661b469d548ae3b6
js177 <126610840+josousa177@users.noreply.github.com>
2025-03-26 19:19:39 +0000

    mains

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,5 +19,12 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+    }
+
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+
     }
 }
