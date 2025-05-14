738035953ccaa40a73b596d77760c303413120c8
João Sousa <joao1666marinho@gmail.com>
2025-05-14 17:04:23 +0100

    adiçao de voz

--- ViewModel/StartGameViewModel.cs
+++ ViewModel/StartGameViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BattleshipAudioGame.Serviços;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,23 +10,62 @@ using System.Windows.Input;
 
 namespace BattleshipAudioGame.ViewModel
 {
-    public class StartGameViewModel : BaseViewModel 
+    public class StartGameViewModel : BaseViewModel, IDisposable
     {
+
+        private readonly VoiceRecognitionService _voice;
+        private readonly Action<bool> _setAudio;
         private readonly Action<string> _navigate;
 
         public ICommand PlayCommand { get; }
         public ICommand TutorialCommand { get; }
         public ICommand ExitCommand { get; }
 
-        public StartGameViewModel(Action<string> navigate)
+
+        /* ------------- Commands ------------- */
+        public ICommand PlayAudioCommand { get; }
+        public ICommand PlaySilentCommand { get; }
+        public ICommand ExitAudioCommand { get; }
+
+        public StartGameViewModel(Action<string> navigate, Action <bool> setAudioFlag)
         {
+
             _navigate = navigate;
+            _setAudio = setAudioFlag;
+            _voice = new VoiceRecognitionService();
+
+            /* ---------- botões (opcionais) ---------- */
+            PlayAudioCommand = new RelayCommand(_ => StartGame(true));
+            PlaySilentCommand = new RelayCommand(_ => StartGame(false));
+            ExitAudioCommand = new RelayCommand(_ => _navigate("Exit"));
+
+            /* ---------- diálogo de voz ---------- */
+            _voice.OnCommand += HandleIntroVoice;
+            _voice.StartRecognition(new[] { "áudio", "silêncio", "sair" });
+            _voice.Speak("Bem‑vindo ao Batalha Naval. "
+                       + "Deseja jogar com áudio? Diga 'áudio' ou 'silêncio'.");
 
             PlayCommand = new RelayCommand(_ => _navigate("Placement"));
             TutorialCommand = new RelayCommand(_ => ShowTutorial());
-            ExitCommand = new RelayCommand(_ => _navigate("Exit"));
+            ExitAudioCommand = new RelayCommand(_ => _navigate("Exit"));
         }
-        
+
+        private void HandleIntroVoice(string cmd)
+        {
+            if (cmd.Contains("áudio")) StartGame(true);
+            else if (cmd.Contains("silêncio")) StartGame(false);
+            else if (cmd.Contains("sair")) _navigate("Exit");
+        }
+
+        private void StartGame(bool audio)
+        {
+            _setAudio(audio);          // grava escolha global
+            _voice.StopRecognition();  // menu acabou
+            _navigate("Placement");
+        }
+
+        public void Dispose() => _voice.Dispose();
+
         private void ShowTutorial()
         {
             MessageBox.Show("Tutorial placeholder (vamos substituir por voz depois).");
